d51251eb39b889898a35bcfea6e6e76e9f7a3c58
Mindcrank <tarynfehr@gmail.com>
2022-02-23 01:13:01 -0600

    1 file modified

--- Components/BuildCard.tsx
+++ Components/BuildCard.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CardHeader from 'react-bootstrap/esm/CardHeader';
 
 function BuildCard() {
   var element = (
     <Card style={{ width: '18rem' }}>
-      <FontAwesomeIcon
-        icon="fa-solid fa-chess-rook fa-border"
-        size="10x"
-        border="40% 10%"
-      />
+      <CardHeader>Martial School</CardHeader>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <FontAwesomeIcon
+          icon={['fas', 'chess-rook']}
+          size="10x"
+          style={{'--fa-border-radius':"10% 40%"}}
+          border
+
+        />
+        <Card.Title>Bulwark</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
